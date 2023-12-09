bc0e0f2a67865ec8ca2bb5632cea386d40e490fa
Klimanova Evgenia <mgenya22@gmail.com>
2023-12-09 16:55:01 +0300

    add checkstye and add fake test

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void emptyTest(){
+        Assertions.assertEquals(1, 1);
+    }
+
+}
