23b8b8d14e8aaf04fdb8446519430d8bb50ed3a3
Klimanova Evgenia <mgenya22@gmail.com>
2023-12-13 22:22:37 +0300

    Fix checkstyle error

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,36 +1,36 @@
 package hexlet.code;
 
 public class StringSchema {
-    private int minLength;
-    private String substring;
-    private boolean required;
+    private int minLengthRule;
+    private String substringRule;
+    private boolean requiredRule;
 
     public boolean isValid(String str) {
         boolean check = true;
-        if (this.required) {
+        if (this.requiredRule) {
             check = check && !(str == null || str.isEmpty());
         }
-        if (this.minLength != 0) {
-            check = check && (str.length() >= this.minLength);
+        if (this.minLengthRule != 0) {
+            check = check && (str.length() >= this.minLengthRule);
         }
-        if (this.substring != null) {
-            check = check && str.contains(substring);
+        if (this.substringRule != null) {
+            check = check && str.contains(this.substringRule);
         }
         return check;
     }
 
     public final StringSchema required() {
-        this.required = true;
+        this.requiredRule = true;
         return this;
     }
 
     public final StringSchema minLength(int minLength) {
-        this.minLength = minLength;
+        this.minLengthRule = minLength;
         return this;
     }
 
     public final StringSchema contains(String substring) {
-        this.substring = substring;
+        this.substringRule = substring;
         return this;
     }
 }
